6f1cc3e49510c72c2db9f33ed85e2db4f69ac99d
Fuzail Ahemd <fuzailcode@gmail.com>
2025-11-29 22:17:07 +0500

    update 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import { MyRoutes } from './routes/MyRoutes'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-
 const App = () => {
-
   useEffect(() => {
     AOS.init({
       duration: 1000,
